21218caa17454a0400231838636c62c07b8e1b1e
ronito101 <122494828+ronito101@users.noreply.github.com>
2023-11-17 13:07:16 -0800

    Update StudentCheckList.kt

--- app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/StudentCheckList.kt
+++ app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/StudentCheckList.kt
@@ -1,11 +1,42 @@
 package a.co.varsitycollege.st10090538.xbcad_poe
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Date
 
 class StudentCheckList : AppCompatActivity() {
+
+    private lateinit var milestoneEditText: EditText
+    private lateinit var addMilestoneButton: Button
+    private lateinit var dbHelper: DBHelper
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.student_checklist)
+
+           milestoneEditText = findViewById(R.id.editTextText2)
+        addMilestoneButton = findViewById(R.id.button2)
+
+        // Initialize DBHelper
+        dbHelper = DBHelper()
+
+        // Set click listener for the "Add Milestone" button
+        addMilestoneButton.setOnClickListener {
+            addMilestone()
+    }
+}
+    private fun addMilestone() {
+        // Get the entered milestone text
+        val milestoneText = milestoneEditText.text.toString()
+
+        val date = Date()
+
+        // Using dbHelper to perform database operation
+        dbHelper.saveAnnouncement("username", "Milestone Title", milestoneText,java.sql.Date(date.time))
+
+        // Clear the EditText after adding the milestone
+        milestoneEditText.text.clear()
     }
 }
