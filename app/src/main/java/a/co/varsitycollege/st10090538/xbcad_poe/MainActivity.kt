e450897f3522de9569a7ec5fed65d03f68abc783
SBezuidenhout25 <102740824+SBezuidenhout25@users.noreply.github.com>
2023-11-17 20:50:13 +0200

    Login bug fixed?

--- app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/MainActivity.kt
+++ app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        GlobalData.loggedInUser = null
         HttpsURLConnection.setDefaultSSLSocketFactory(TLSSocketFactory())
         findViewById<TextView>(R.id.registerText).setOnClickListener(){
             startActivity(Intent(this, Register::class.java))
--- app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/StudentGroupList.kt
+++ app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/StudentGroupList.kt
@@ -16,6 +16,7 @@ class StudentGroupList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.student_group_list)
+
     }
 
 }
