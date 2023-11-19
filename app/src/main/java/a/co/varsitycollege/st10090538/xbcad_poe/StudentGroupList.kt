9da0f9d10d59df9869da8d98ca51ebda83de9e1f
ronito101 <122494828+ronito101@users.noreply.github.com>
2023-11-19 07:41:30 -0800

    Update StudentGroupList.kt

--- app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/StudentGroupList.kt
+++ app/src/main/java/a/co/varsitycollege/st10090538/xbcad_poe/StudentGroupList.kt
@@ -5,20 +5,109 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.concurrent.Executors
+
 
 
 class StudentGroupList : AppCompatActivity() {
+
+    private val dbHelper = DBHelper()
+    
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.student_group_list)
 
+         val groupRecyclerView = findViewById<RecyclerView>(R.id.groupRecyclerView)
+        groupRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Fetch student groups from the database
+        Executors.newSingleThreadExecutor().execute {
+            dbHelper.getStudentGroups().start()
+
+               val groupList = GlobalData.studentGroupList
+
+            runOnUiThread {
+                // Create and set the adapter
+                val groupAdapter = GroupAdapter(groupList)
+                groupRecyclerView.adapter = groupAdapter
+            }
+        }
+        val createGroupButton = findViewById<Button>(R.id.createGroupButton)
+        createGroupButton.setOnClickListener {
+            // Show a dialog to get the group name
+            showGroupNameInputDialog()
+        }
+
     }
 
+@RequiresApi(Build.VERSION_CODES.O)
+    private fun showGroupNameInputDialog() {
+
+        val inputGroupName = EditText(this)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Enter Group Name")
+            .setView(inputGroupName)
+            .setPositiveButton("Create") { _, _ ->
+                val groupName = inputGroupName.text.toString()
+
+                // Call the method to add the new group to the database
+                addNewGroupToDatabase(groupName)
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }
+            .create()
+
+        dialog.show()
+    }
+
+     @RequiresApi(Build.VERSION_CODES.O)
+    private fun addNewGroupToDatabase(groupName: String) {
+        Executors.newSingleThreadExecutor().execute {
+            dbHelper.addGroup(groupName) { success ->
+
+                runOnUiThread {
+                    if (success) {
+                        // Refresh the group list after adding the new group
+                        refreshGroupList()
+                    } else {
+                        Toast.makeText(this, "Failed to create a new group", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private fun refreshGroupList() {
+        // Fetch student groups from the database asynchronously
+        Executors.newSingleThreadExecutor().execute {
+            dbHelper.getStudentGroups().start()
+
+
+            val groupList = GlobalData.studentGroupList
+
+            runOnUiThread {
+                // Update the existing adapter with the new group list
+                val groupAdapter = GroupAdapter(groupList)
+
+                val groupRecyclerView = findViewById<RecyclerView>(R.id.groupRecyclerView)
+                groupRecyclerView.adapter = groupAdapter
+            }
+        }
+    }
 }
+
 
 
